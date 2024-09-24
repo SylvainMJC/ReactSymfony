084eda8c5ea46016f858c4aee8461f8db2eb1b19
Yoni Deserbaix <yonideserbaix@gmail.com>
2024-09-24 15:10:29 +0200

    fix(button): add className as props

--- assets/react/controllers/components/Button.jsx
+++ assets/react/controllers/components/Button.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-export default function Button({ title }) {
+export default function Button({ title, className }) {
   return (
-    <div className="text-white text-md bg-primary rounded-md px-4 py-2">
+    <div
+      className={`text-white text-md bg-primary rounded-md px-4 py-2 ${className} max-w-max`}
+    >
       {title}
     </div>
   );
