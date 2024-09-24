0fe135d391f3ef461094c6e24fd3c174c33e4567
Yoni Deserbaix <yonideserbaix@gmail.com>
2024-09-24 14:30:35 +0200

    code into jsx

--- assets/react/controllers/Hello.jsx
+++ assets/react/controllers/Hello.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function () {
-  return <div>Hello world</div>;
+  return (
+    <div class="bg-red-500">
+      <div class="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div class="text-center">
+          <p class="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
+            Ceci est une démo de React dans une application Symfony
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
