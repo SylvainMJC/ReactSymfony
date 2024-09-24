29684848904485c9beaabc3f82e9eeaf51975c86
Yoni Deserbaix <yonideserbaix@gmail.com>
2024-09-24 19:47:50 +0200

    few updates

--- webpack.config.js
+++ webpack.config.js
@@ -72,18 +72,18 @@ Encore
   // uncomment if you're having problems with a jQuery plugin
   //.autoProvidejQuery()
 
-  .copyFiles({
-    from: "./assets/images",
+  // .copyFiles({
+  //   from: "./assets/images",
 
-    // optional target path, relative to the output dir
-    to: "images/[path][name].[ext]",
+  //   // optional target path, relative to the output dir
+  //   to: "images/[path][name].[ext]",
 
-    // if versioning is enabled, add the file hash too
-    //to: 'images/[path][name].[hash:8].[ext]',
+  //   // if versioning is enabled, add the file hash too
+  //   //to: 'images/[path][name].[hash:8].[ext]',
 
-    // only copy files matching this pattern
-    //pattern: /\.(png|jpg|jpeg)$/
-  })
+  //   // only copy files matching this pattern
+  //   //pattern: /\.(png|jpg|jpeg)$/
+  // })
   .enablePostCssLoader();
 
 // .configureWatchOptions(function(watchOptions) {
