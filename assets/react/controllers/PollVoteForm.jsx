5cf0baed990f3fc0d86055024fec76aad39158f7
Sylvain Conan <sylvain.mj.conan@gmail.com>
2024-09-26 08:59:06 +0200

    fixed PolleVoteForm

---
+++ assets/react/controllers/PollVoteForm.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+export default function (props) {
+  // console.log(props.htmlForm);
+  return (
+    <form name="vote_poll" method="post">
+      <div>
+        <label class="required">Answer choice</label>
+        <div id="poll_vote_answerChoice">
+          {/* todo : foreach answer as answer */}
+          {/* todo : typage multiple ou unique (radio ou checkbox) */}
+          <input
+            id="poll_vote_answerChoice_0"
+            name="poll_vote[answerChoice]"
+            required="required"
+            value="9"
+            type="radio"
+          />
+          <label for="poll_vote_answerChoice_0" class="required">
+            Rouge
+          </label>
+
+          <input
+            type="radio"
+            id="poll_vote_answerChoice_1"
+            name="poll_vote[answerChoice]"
+            required="required"
+            value="10"
+          />
+          <label for="poll_vote_answerChoice_1" class="required">
+            Bleu
+          </label>
+
+          <input
+            type="radio"
+            id="poll_vote_answerChoice_2"
+            name="poll_vote[answerChoice]"
+            required="required"
+            value="11"
+          />
+          <label for="poll_vote_answerChoice_2" class="required">
+            Vert
+          </label>
+
+          <input
+            type="radio"
+            id="poll_vote_answerChoice_3"
+            name="poll_vote[answerChoice]"
+            required="required"
+            value="12"
+          />
+          <label for="poll_vote_answerChoice_3" class="required">
+            Jaune
+          </label>
+        </div>
+      </div>
+      <button class="btn">Voter</button>
+      <input
+        type="hidden"
+        id="poll_vote__token"
+        name="poll_vote[_token]"
+        value="d10a.gx6eC4CFsSQVhgDOJLAXE3dtXVjaQS60iBk9iRmOcJQ.4lboZPXyhGp20GiRTIZEQU4PNBSKGGLn3X9rsXujR9u0TLNmrdf_YiXjbw"
+      ></input>
+    </form>
+  );
+}
